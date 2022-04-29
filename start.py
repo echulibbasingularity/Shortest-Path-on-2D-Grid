3861f4078cbf41005b00928d3040524a0ca312f1
Essa <77778010+echulibbasingularity@users.noreply.github.com>
2022-04-29 20:11:59 +0500

    Update start.py

--- start.py
+++ start.py
@@ -1,17 +1,20 @@
 
+
 import pygame
 
 WIDTH,HEIGHT = 800,800
 ROWS,COL = 8,8
 SIZE = WIDTH//COL  # SIZE OF SQUARE
 
-fps = 100
+fps = 70
 
-PINK = (253,87,231)   #RGB COORDINATES
+# RGB COORDINATES
+'''wow'''
+PINK = (253,87,231)
 WHITE = (240,219,230)
 
 image = pygame.image.load('chess.png')
-image = pygame.transform.scale(image, (SIZE-10,SIZE-10))  #RESIZING IMAGE
+image = pygame.transform.scale(image, (SIZE-10,SIZE-10))  # RESIZING IMAGE
 
 pic_X, pic_Y = 0,0
 
@@ -24,13 +27,32 @@ def grid():
     for row in range(ROWS):
         for col in range(row%2,ROWS,2):
             pygame.draw.rect(screen,PINK,(col*SIZE, row*SIZE, SIZE,SIZE))
-    
-def knight_move(pic_X, pic_Y):
-    if pic_X==0 and pic_Y==0:
-        # draws the image on the surface at (x,y)
-        screen.blit(image,(pic_X, pic_Y))
+
+def curr_pos(x, y):
+    return (x,y)
+
+def legal_move(x, y):
+    if x<0 or y<0 or x>WIDTH or y>WIDTH:
+        return False
     else:
-        screen.blit(image,(pic_X-48, pic_Y-47))
+        return True
+
+def knight_move(pic_X, pic_Y):
+    # x_co = curr_pos(x,y)[0]
+    # y_co = curr_pos(x,y)[1]
+    # dx = [-200,-200,100,200]
+    # dy=[-100,100,200,100]
+    # for i in range(2):
+    #     new_X = pic_X + dx[i]
+    #     new_Y = pic_Y + dy[i]  
+
+
+
+
+    # else:
+
+
+        screen.blit(image,(pic_X, pic_Y))
              
     
 
@@ -52,11 +74,15 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pic_X,pic_Y = pygame.mouse.get_pos()  # returns x,y position of the mouse when clicked
+            print(pic_X,pic_Y)
             
 
 
 
     grid()
-    knight_move(pic_X,pic_Y)
+    #curr_pos((pic_X//SIZE)*SIZE + SIZE/2 - 48,(pic_Y//SIZE)*SIZE + SIZE/2 - 47)
+    knight_move((pic_X//SIZE)*SIZE + SIZE/2 - 48,(pic_Y//SIZE)*SIZE + SIZE/2 - 47)
+    
     pygame.display.update()
 pygame.display.quit()
+
